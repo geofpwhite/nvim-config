ba81896b9ebbecf93271d585fb26ad9b6d8c1364
Geoffrey White <87089088+geoffreypwhite@users.noreply.github.com>
2023-03-20 23:16:58 -0400

    _

--- init.lua
+++ init.lua
@@ -1,5 +1,34 @@
 return {
   -- Configure AstroNvim updates
+  highlights = {
+    -- set highlights for all themes
+    -- use a function override to let us use lua to retrieve colors from highlight group
+    -- there is no default table so we don't need to put a parameter for this function
+    init = function()
+      local get_hlgroup = require("astronvim.utils").get_hlgroup
+      -- get highlights from highlight groups
+      local normal = get_hlgroup "Normal"
+      local fg, bg = normal.fg, normal.bg
+      local bg_alt = get_hlgroup("Visual").bg
+      local green = get_hlgroup("String").fg
+      local red = get_hlgroup("Error").fg
+      -- return a table of highlights for telescope based on colors gotten from highlight groups
+      return {
+        TelescopeBorder = { fg = bg_alt, bg = bg },
+        TelescopeNormal = { bg = bg },
+        TelescopePreviewBorder = { fg = bg, bg = bg },
+        TelescopePreviewNormal = { bg = bg },
+        TelescopePreviewTitle = { fg = bg, bg = green },
+        TelescopePromptBorder = { fg = bg_alt, bg = bg_alt },
+        TelescopePromptNormal = { fg = fg, bg = bg_alt },
+        TelescopePromptPrefix = { fg = red, bg = bg_alt },
+        TelescopePromptTitle = { fg = bg, bg = red },
+        TelescopeResultsBorder = { fg = bg, bg = bg },
+        TelescopeResultsNormal = { bg = bg },
+        TelescopeResultsTitle = { fg = bg, bg = bg },
+      }
+    end,
+  },
   updater = {
     remote = "origin", -- remote to use
     channel = "stable", -- "stable" or "nightly"
@@ -63,11 +92,9 @@ return {
   -- augroups/autocommands and custom filetypes also this just pure lua so
   -- anything that doesn't fit in the normal config locations above can go here
   polish = function()
-    vim.keymap.set("n", "s", "ysiw")
     vim.keymap.set("n", "<leader>\\", ":split | Telescope buffers<cr>")
     vim.keymap.set("n", "<leader>|", ":vsplit|Telescope buffers<cr>")
     vim.cmd ":nmap s ysiw"
-    -- vim.cmd ":nmap <leader>\\ <cmd>split | Telescope buffers <cr>"
     -- Set up custom filetypes
     -- vim.filetype.add {
     --   extension = {
