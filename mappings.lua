d5d0393dda958859d9adcb6db54bd23bd7183523
Geoffrey White <87089088+geoffreypwhite@users.noreply.github.com>
2023-03-21 15:38:36 -0400

    _

--- mappings.lua
+++ mappings.lua
@@ -17,11 +17,13 @@ return {
     ["f"] = { "<cmd>HopWordMW<cr>" },
     ["gr"] = { function() vim.lsp.buf.references() end },
     ["<leader>\\"] = { "<cmd>split | Telescope buffers <cr>" },
-    ["<leader><S-\\>"] = { "<cmd>vsplit | Telescope buffers <cr>" },
+    -- ["<leader><S-\\>"] = { "<cmd>vsplit | Telescope buffers <cr>" },
     ["<leader>o"] = { "<cmd>AerialToggle<cr>", desc = "symbol outline" },
     ["<leader>bn"] = { "<cmd>tabnew<cr>", desc = "New tab" },
     ["<leader>bd"] = { "<cmd>WintabsClose<cr>" },
+    ["<leader>Q"] = { ":qa!<cr>" },
     ["<leader>bD"] = {
+
       function()
         require("astronvim.utils.status").heirline.buffer_picker(
           function(bufnr) require("astronvim.utils.buffer").close(bufnr) end
